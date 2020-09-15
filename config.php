1bec16c2992903f0b24bfdea0fe6838213624ec0
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-16 00:18:56 +0530

    Config: Db Config

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+ob_start();
+
+
+// Config Site Basic Data 
+$siteName = "E-Kommerce";
+$tagLine1 = "E-Kommerce: Buy goods Online";
+
+
+// DATABASE Connection
+$servername = "localhost";
+$username = "addkon7l_card";
+$password = "9]U#0&rVBFT";
+$database = "ecomsite";
+
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+//echo "Database Connected successfully";
