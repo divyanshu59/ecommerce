afed44d4a0059275f71212afabfa4e386d737e7a
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-18 17:32:43 +0530

    Add: Change Status of users

---
+++ admin/changeuserstatus.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../config.php';
+
+if (!isset($_COOKIE['adminlogin'])) {
+    header('Location: login.php');
+    die("Please Wait You are Rediritig..");
+} else {
+    $name = $_COOKIE['adminname'];
+    $email = $_COOKIE['adminemail'];
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $status = $_GET['to'];
+
+    $sql = "UPDATE `users` SET `status`='$status' WHERE `id` = '$id'";
+    $queryRun = mysqli_query($con, $sql);
+}
+header('Location: users.php');
