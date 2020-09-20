42a7fe80627cf088583a1e65da9719c3742f159d
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-20 23:32:00 +0530

    Add: Thankyou Page

---
+++ thank.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once 'config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    if (!isset($_COOKIE['userlogin'])) {
+        header('Location: login.php');
+    }
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $siteName; ?></title>
+
+    <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="asset/css/index.css">
+</head>
+
+<body>
+    <header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">
+            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                <span class="mdc-top-app-bar__title"> <?php echo $siteName; ?></span>
+
+
+            </section>
+            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+                <form id="searchBar" action="search.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit" class="material-icons">search</button>
+                </form>
+                <a class="material-icons mdc-top-app-bar__action-item mdc-icon-button" href="cart.php" aria-label="Search">shopping_cart</a>
+                <?php
+                if (isset($_COOKIE['userlogin'])) {
+                    echo '<a class="material-icons mdc-top-app-bar__action-item mdc-icon-button" href="dashboard.php" aria-label="Options">dashboard</a>';
+                } else {
+                    echo '<a class="material-icons mdc-top-app-bar__action-item mdc-icon-button" href="login.php" aria-label="Options">login</a>';
+                }
+                ?>
+
+            </section>
+        </div>
+    </header>
+    <br><br>
+    <br><br>
+    <center>
+        <img src="asset/image/thankyou.jpg" alt="" srcset="">
+        <br>
+        <a id="myAccBtn" href="dashboard.php">Goto: My Account</a>
+
+    </center>
+
+
+</body>
+
+</html>
