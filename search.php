150e7882add22bd1721c42f6c7e59bd1257577fd
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-20 19:58:21 +0530

    Add: Search Page

--- search.php
+++ search.php
@@ -48,7 +48,58 @@ if (isset($_GET['search'])) {
             </section>
         </div>
     </header>
+    <br><br>
+    <br><br>
+    <br>
+    <div class="row">
+        <div class="col-8 main">
 
+            <?php
+            $sql = "SELECT * FROM `products` WHERE `name` LIKE '%$search%' OR  `name` LIKE '$search%' OR  `name` LIKE '%$search'  ";
+            $queryRun = mysqli_query($con, $sql);
+            if (mysqli_num_rows($queryRun) > 0) {
+                while ($row = mysqli_fetch_array($queryRun)) {
+                    $url = "";
+                    $catid = $row[4];
+
+                    $sql2 = "SELECT * FROM `category` WHERE `id` = '$catid' ";
+                    $queryRun2 = mysqli_query($con, $sql2);
+                    if (mysqli_num_rows($queryRun2) > 0) {
+                        while ($row2 = mysqli_fetch_array($queryRun2)) {
+                            $category = $row[1];
+                        }
+                    }
+
+                    if ($row[7] != null) {
+                        $images = explode(',', $row[7]);
+                        $url = $images[0];
+                    } else {
+                        $url = "asset/image/product.jpg";
+                    }
+
+                    echo "
+                    <div class='my-card-content mdc-card'>
+                    <div class='row'>
+                        <div class='col-3'>
+                            <img src='$url' width='100%'>
+                        </div>
+                        <div class='col-9'>
+                            <h2>$row[1]</h2>
+                            <samp>Category: $category</samp>
+                            <h4>Cost: ₹ $row[6]</h4>
+                        </div>
+                    </div>
+                    </div>
+                    ";
+                }
+            }
+
+            ?>
+
+
+
+        </div>
+    </div>
 
 </body>
 
